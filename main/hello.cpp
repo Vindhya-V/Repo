8a1ffd453084f42e5a5a919df79ee609bdaa667d
Vindhya-V <113531189+Vindhya-V@users.noreply.github.com>
2023-02-15 12:32:55 +0530

    Describe this commit

---
+++ main/hello.cpp
@@ -0,0 +1,14 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS612\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
+
+
+
