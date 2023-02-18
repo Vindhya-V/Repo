6c87049a8aa0cf52b6edd0d446fddc9ed101158f
Vindhya-V <113531189+Vindhya-V@users.noreply.github.com>
2023-02-18 14:23:17 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG20CS612\n";
+  cout << "SRN: PES1UG20CS612 Name: Vindhya V\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
